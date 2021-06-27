312d2637d1efe28e332bf41df16380c9a58b14b8
Burak GÖK <79623899+gokburak@users.noreply.github.com>
2021-06-27 23:57:14 +0300

    add api

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { AppBar,Toolbar,IconButton,Badge,MenuItem,Menu,Typography} from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assets/commerce.png'
-import useStyles from './styles';
+import useStyles from '../Product/styles';
 
 
 
