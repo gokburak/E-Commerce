3d3d7448f8bec795b7c95d96ca09b4fa04c772bf
Burak GÖK <79623899+gokburak@users.noreply.github.com>
2021-06-28 23:26:50 +0300

    add products

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ import { commerce } from './lib/commerce';
 
 
 const App = () => {
+
   const [products,setProducts]=useState([]);
 
-    const fetchProducts = async(){
-      const {data} = await commerce.products.list();
+    const fetchProducts = async () =>{
+      
+      const {data}= await commerce.products.list();
 
       setProducts(data); 
     }
     useEffect(() =>{
       fetchProducts()
     },[]);
+
+    console.log(products);
+
     return (
         <div>
           <Navbar/>
